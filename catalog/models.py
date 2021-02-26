e9931fa088497cef612c8a01865808eb8473e5b8
daniel cumming <cumming.romo.daniel@gmail.com>
2021-02-26 15:29:13 -0400

    initial project skeleton - no models have been added

---
+++ catalog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+"""
+Everytime a model is changed in a way that affects the structure of data
+Run:
+python3 manage.py makemigrations
+python3 manage.py migrate
+"""
+
