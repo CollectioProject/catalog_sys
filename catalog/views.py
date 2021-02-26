7563129ddcbeb6e3dc2fe04bfc9d338a11e67aa6
daniel cumming <cumming.romo.daniel@gmail.com>
2021-02-26 18:17:37 -0400

    first complete skeleton with records and display

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from . import models
+
+from django.forms.models import model_to_dict
 
 # Create your views here.
+posts = [
+    {
+        'id': '1230314',
+        'title':'Antique Piece 1',
+        'description': 'a nice piece of a chair',
+        'created':'January 20, 2021',
+        'updated':'January 21, 2021',
+    },
+    {
+        'id': '22222222',
+        'title':'Antique Piece 2',
+        'description': 'a nice piece of a chair',
+        'created':'February 20, 2021',
+        'updated':'February 21, 2021',
+    }
+]
+
+# def home(request):
+#     return render(request, 'catalogues/home.html', {'title': 'Home'})
+
+
+def home(request):
+    
+    posts = models.Record.objects.all()
+    context = {
+        'posts': posts,
+    }
+    # context = models.Record.objects.all()
+    # context = [result for result in results]
+
+    # context = [{'posts': i} for i in models.Record.objects.all()]
+
+    # if results is not None:
+    # context = model_to_dict(results)
+    # else:
+    #     context = None
+
+    # render works in such a way that it gets the home.html file from the blog folder in templates.
+    return render(request, 'catalog/home.html', context )
+
--- catalog_sys/urls.py
+++ catalog_sys/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
 # Use static() to add url mapping to serve static files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  
 # ----------------------------------------------------
