503557dbfe629a103d3a1da5760fa271374df351
daniel cumming <cumming.romo.daniel@gmail.com>
2021-03-14 21:36:47 -0300

    reset database with new superuser dan,  added CommonInfo model, and Catalog model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from django.db import models
 from django.db.models.fields import CharField
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
@@ -6,28 +7,48 @@ import uuid
 # Create your models here.
 """
 Everytime a model is changed in a way that affects the structure of data
-Run:
-python3 manage.py makemigrations
-python3 manage.py migrate
+    python3 manage.py makemigrations
+    python3 manage.py migrate
+
+When creating models register them on admin.py
+
+To reset/clear database
+1) Remove all migrations files from within project
+    find . -path "*/migrations/*.py" -not -name "__init__.py" -delete;
+    find . -path "*/migrations/*.pyc"  -delete
+2) Delete database db.sqlite3
+3) Create inital migrations and generate database: makemigrations; migrate;
 """
 
+class CommonInfo(models.Model):
+    id = models.AutoField(primary_key=True) # not necessary as django adds this to every model, but declared so that it is clear
+    creation_date = models.DateField(auto_now_add=True)
+    last_modified = models.DateField(auto_now=True)
 
-class Record(models.Model):
-    # Fields
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, help_text='The Unique ID of record')
-    title = models.CharField(max_length=200, help_text='Enter name/title of object')
-    description = models.TextField(null=True,blank=True, help_text='Enter a short description of the object on record')
-    created = models.DateField(auto_now_add=True, help_text='Date of initial record creation')
-    updated = models.DateField(auto_now=True)
-
-    # Metadata
+    name = models.CharField(max_length=100, help_text='Enter name')
+    description = models.TextField(blank=True, help_text='Enter description')
+    
     class Meta:
-        ordering = ['updated']
+        abstract = True
+        ordering = ['name','-last_modified'] # '-' reverses order, e.i. newest first
+
+class Catalog (CommonInfo):
+
+    def get_absolute_url(self):
+        return reverse('catalog-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
+    
+
+class Record(CommonInfo):
+    my_catalog = models.ForeignKey(Catalog, on_delete=models.CASCADE) # Many records to one Catalog. Deletes all records associated with deleted catalog.
+    
 
     # Methods
     def get_absolute_url(self):
         return reverse('record-detail', args=[str(self.id)])
 
     def __str__(self):
-        return f'{self.title} ({self.id})'
+        return f'{self.name} ({self.my_catalog})'
     pass
